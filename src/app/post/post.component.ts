7af623fe00d4fa5af7924f42e9e6ed7fb1285162
Mardixx <kimi.lefort.pro@gmail.com>
2024-03-01 16:00:51 +0100

    Weather infos displayed design to do

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { WeatherArray } from '../../shared/modules/weatherArray';
+
 
 @Component({
   selector: 'post',
@@ -12,17 +12,32 @@ import { WeatherArray } from '../../shared/modules/weatherArray';
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
+// const items : WeatherArray[] {
+
+// }
 export class PostComponent{
   @Output() fetch = new EventEmitter<string>();
 
 	http = inject(HttpClient); //Fetching in ....component.ts
 	posts : any = [];
-  inputField = "";
+  response : any = [];
+  weather : any = [];
+  inputField : string = "";
+
 	fetchPosts() {
 		this.http.get("https://api.openweathermap.org/data/2.5/forecast?&units=metric&q=" + this.inputField + "&appid=")
 		.subscribe((posts : any) => {
-			console.log(posts);
       this.inputField = "";
+      this.weather = [];
+      for(let i = 0; i <= 40; i++) {
+        let str : string = posts.list[i].dt_txt;
+        let noon : boolean = str.includes("12:00:00");
+        if (noon) { 
+          this.response = [];
+          this.response.push(posts);
+          this.weather.push(posts.list[i]);
+        }
+      }
 		});
 	} 
 }
